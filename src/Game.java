c7662f2155bd9e8f7daf53bf46611d8b0199b95e
Sujet17 <joachim.sn@hotmail.com>
2017-03-09 15:01:43 +0100

    create class Game
    première version de la classe game, contenant un tableau 2D contenant des null ou des objets Position  (ou héritant de)

---
+++ src/Game.java
@@ -0,0 +1,19 @@
+
+public class Game {
+	Position [][] matrix;
+	public Game (int height, int width) {
+		matrix = new Position [height][width];
+	}
+	public int getHeight {
+		return height;
+	}
+	public int getWidth {
+		return Width;
+	}
+	public boolean isFree (int posX, int posY) {
+		if ((posX < height) && (posY < width)):
+			//retourne un rensignement sur ce que contient la case concernee
+		else:
+			// Erreur de OUF
+	}
+}
