1c2414878f60392211c1452158c8313d66000ed5
Sujet17 <joachim.sn@hotmail.com>
2017-03-09 14:35:05 +0100

    create Position.java
    1 ère version de la classe Position

---
+++ src/Position.java
@@ -0,0 +1,14 @@
+
+public class Position {
+	int x, y;
+	public Position (int inputX, int inputY) {
+		x = inputX;
+		y = inputY;
+	}
+	public int getX () {
+		return x;
+	}
+	public int getY () {
+		return y;
+	}
+}
