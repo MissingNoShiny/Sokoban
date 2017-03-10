3f2bd740c92d9a4349f325e2dae6687a7f7a4f7c
Vincent Larcin <vincent.larcin@student.umons.ac.be>
2017-03-10 10:40:47 +0100

    Javadoc Position.java
    Rédaction de la javadoc pour la classe Position. Elle devra
    potentiellement être modifiée par la suite.

--- src/Position.java
+++ src/Position.java
@@ -1,24 +1,74 @@
 
+/**
+ * Description
+ * @author Vincent Larcin, Joachim Sneessens
+ */
 public class Position {
-	int x, y;
+	
+	/**
+	 * The X-coordinate of the object.
+	 */
+	private int x;
+	
+	/**
+	 * The Y-coordinate of the object.
+	 */
+	private int y;
+	
+	/**
+	 * Creates a position with the specified coordinates in input.
+	 * @param inputX the X-coordinate of the object
+	 * @param inputY the Y-coordinate of the object
+	 */
 	public Position (int inputX, int inputY) {
 		x = inputX;
 		y = inputY;
 	}
 	
+	/**
+	 * Gets the current position of the object on the X-axis.
+	 * @return The X-coordinate of the object
+	 */
 	public int getX () {
 		return x;
 	}
 	
+	/**
+	 * Gets the current position of the object on the Y-axis.
+	 * @return The Y-coordinate of the object
+	 */
 	public int getY () {
 		return y;
 	}
 	
+	/**
+	 * Sets the X-coordinate of the object to a new value.
+	 * @param inputX the new X-coordinate
+	 */
 	public void setX(int inputX) {
 		x = inputX;
 	}
 	
+	/**
+	 * Sets the Y-coordinate of the object to a new value.
+	 * @param inputY the new Y-coordinate
+	 */
 	public void setY(int inputY) {
 		y = inputY;
+	}
+	
+	/**
+	 * Compares the coordinate of the object with the coordinates of another.
+	 * @param other the object to compare the coordinates with
+	 * @return true if the objects have the same coordinates, false else
+	 */
+	public boolean equals (Position other) {
+			
+		if (this.x != other.x)
+			return false;
+		if (this.y != other.y)
+			return false;
+		
+		return true;		
 	}
 }
