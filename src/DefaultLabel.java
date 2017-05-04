d1bcb6015e13565952e39570ee6a4d0b3fd26f19
MissingNoShiny <larcinvincent@gmail.com>
2017-05-04 13:39:56 +0200

    Update and rename defaultLabel.java to DefaultLabel.java

--- src/DefaultLabel.java
+++ src/DefaultLabel.java
@@ -1,11 +1,11 @@
 
 import javax.swing.JLabel;
 
-public class defaultLabel extends JLabel {
+public class DefaultLabel extends JLabel {
 
 	private static final long serialVersionUID = -2925126198019609830L;
 	
-	public defaultLabel(String string) {
+	public DefaultLabel(String string) {
 		super(string);
 		setBackground(Menu.defaultColor);
 		setOpaque(true);
